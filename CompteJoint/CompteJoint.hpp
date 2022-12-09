ee54a88a6a64dc3aa79f46dc9ade056b85c2c53f
Ridouane <ridouaneaboubacarvisio8092@gmail.com>
2022-12-09 07:15:06 +0100

    style:un repertoire pour chaque classe(.hpp && .cpp) en laissant visible la fonction principale

---
+++ CompteJoint/CompteJoint.hpp
@@ -0,0 +1,9 @@
+#include "CompteBancaire.hpp"
+class CompteJoint :public CompteBancaire
+{
+	private:
+		string Titulaire2;
+	public:
+		CompteJoint(string,string,float);
+		void AfficherCompteBancaire();
+		void 
