c9a1d464f6954b995667d20dfa99a666ca1d31ec
Alazi <alaziissoufoudodo@gmail.com>
2022-12-12 11:22:52 +0100

    feat :CompteEpargne.hp) : initiation de la bibliotheque classe CompteEpargne.hpp

---
+++ CompteEpargne/CompteEpargne.hpp
@@ -0,0 +1,13 @@
+#ifndef CompteEpargne_HPP_
+#define CompteEpargne_HPP_
+#include "CompteBancaire.hpp"
+
+    class CompteEpargne : public ComppteBancaire
+    {
+        private:
+
+        public:
+
+    };
+
+#endif
