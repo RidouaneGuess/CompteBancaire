9669f9b29d7b3fbe671c0183b0f8a25d265151d5
Alazi <alaziissoufoudodo@gmail.com>
2022-12-12 10:54:19 +0100

    feat :CompteTitre.hp) : initiation de la bibliotheque classe CompteTitre.hpp

---
+++ CompteTitre/CompteTitre.hpp
@@ -0,0 +1,12 @@
+#ifndef CompteTitre_HPP_
+#define CompteTitre_HPP_
+#include "CompteBancaire.hpp"
+
+    class CompteTitre : public CompteBancaire
+    {
+        private:
+
+        public:
+            
+    };
+#endif
