69bb236ef93153b5fedbabe05732a8e86f01af0c
iMoubarak <imoubarak862@gmail.com>
2023-01-03 09:14:49 +0100

    ci (CompteBancaire) : creation de la structure de creation de compte bancaire

--- CompteBancaire/CompteBancaire.hpp
+++ CompteBancaire/CompteBancaire.hpp
@@ -2,6 +2,26 @@
 #define COMPTEBANCAIRE_HPP
 #include<string>
 using namespace std;
+struct ccompte
+{
+	//Information de base pour crée un compte
+	string Nom;
+	string Prenom;
+	string Nation;
+	double cNum;
+	int age;
+	int numTel;
+	int cCout;
+	string localite;
+	string type;
+};
+using utilisateur=struct ccompte;
+struct cellule
+{
+	utilisateur user;
+	struct cellule *suiv;
+};
+typedef cellule *liste;
 class CompteBancaire
 {
 	protected:
@@ -9,16 +29,19 @@ class CompteBancaire
 		float Solde;
 		int NombreEnvoi;
 		int NombreRetrait;
+		static liste users;
 	public:
 		CompteBancaire(string,float);
 		void InitialiserCompteBancaire(float);
 		void AccederSoldeCompteBancaire();
 		void CrediterCompteBancaire(float);
-	        void DebiterCompteBancaire(float);
+	    void DebiterCompteBancaire(float);
 		CompteBancaire operator+(CompteBancaire);
 		void operator=(CompteBancaire);
 		virtual void AfficherCompteBancaire();
 		CompteBancaire Virement(float,CompteBancaire);
+		static void creecompte();
+		utilisateur chercherCompte(string Nom,string Prenom,double numCompte);	
 };
 #endif
 
